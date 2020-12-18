00d4b2c62d88c34f8962c9f329e133a48346d30b
ikkei12 <ikkei12.h@gmail.com>
2020-12-18 10:33:02 +0900

    add HeaderItemList

---
+++ components/v1/molecules/NkdHeaderItem/NkdHeaderItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="onItemClick">
+    <span>
+      <p>{{ item.title }}</p>
+    </span>
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object as PropType<HeaderItem>,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const onItemClick = () => {
+      context.emit('onHeaderItemClick', props.item.type)
+    }
+    return { onItemClick }
+  },
+})
+</script>
