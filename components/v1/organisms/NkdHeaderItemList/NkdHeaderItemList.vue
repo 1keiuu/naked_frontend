0fc37cdb8ed515cc084d60c4e896f847933d419c
ikkei12 <ikkei12.h@gmail.com>
2020-12-18 10:33:12 +0900

    add NkdHeaderItem

---
+++ components/v1/organisms/NkdHeaderItemList/NkdHeaderItemList.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <NkdHeaderItem
+      v-for="(item, i) in items"
+      :key="'header-item' + i"
+      :item="item"
+      @onHeaderItemClick="dispatchAction"
+    />
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+import NkdHeaderItem from '@/components/v1/molecules/NkdHeaderItem/NkdHeaderItem.vue'
+export default defineComponent({
+  components: {
+    NkdHeaderItem,
+  },
+  props: {
+    items: {
+      type: Array as PropType<HeaderItem[]>,
+    },
+  },
+  setup(props, context) {
+    const dispatchAction = (type: string) => {
+      context.emit('dispatchAction', type)
+    }
+    return {
+      dispatchAction,
+    }
+  },
+})
+</script>
