6ff78d41645f86ca96dc57d40305074df619f1aa
ikkei12 <ikkei12.h@gmail.com>
2020-12-19 22:19:26 +0900

    fix

---
+++ components/v1/templates/Tasks/TasksPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen logIn-page pt-16 flex flex-col justify-center">
+    <NkdTaskSubHeader />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, inject } from '@vue/composition-api'
+import NkdTaskSubHeader from '@/components/v1/organisms/NkdTasksSubHeader/NkdTasksSubHeader.vue'
+export default defineComponent({
+  components: {
+    NkdTaskSubHeader,
+  },
+})
+</script>
