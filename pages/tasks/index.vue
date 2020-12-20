58c595e9747632e5b54c6f439001421def7f6203
ikkei12 <ikkei12.h@gmail.com>
2020-12-20 14:34:50 +0900

    fix tasks page

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="task-page w-full h-full">
     <TasksProvider>
-      <TasksPage />
+      <TasksPage :today="today" :tomorrow="tomorrow" />
     </TasksProvider>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, reactive } from '@vue/composition-api'
 import TasksProvider from '@/components/v1/providers/Tasks/TasksProvider.vue'
 import TasksPage from '@/components/v1/templates/Tasks/TasksPage.vue'
 
 export default defineComponent({
   components: { TasksProvider, TasksPage },
   setup(_props, context) {
+    const today = reactive<EpicTasks[]>([])
+    const tomorrow = reactive<EpicTasks[]>([])
+
     context.root.$axios
       .get('/api/v1/tasks')
       .then((res) => {
-        console.log(res)
+        today.values = res.data.today
+        tomorrow.values = res.data.tomorrow
       })
       .catch((e) => {
         console.log(e)
       })
+
+    return { today, tomorrow }
   },
 })
 </script>
--- pages/tasks/list.vue
+++ pages/tasks/list.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="task-page w-full h-full">
     <TasksProvider>
-      <TasksPage />
+      <TasksPage :epics="epics" />
     </TasksProvider>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, reactive } from '@vue/composition-api'
 import TasksProvider from '@/components/v1/providers/Tasks/TasksProvider.vue'
 import TasksPage from '@/components/v1/templates/Tasks/TasksPage.vue'
 
 export default defineComponent({
   components: { TasksProvider, TasksPage },
   setup(_props, context) {
+    const epics = reactive<EpicTasks[]>([])
     context.root.$axios
-      .get('/api/v1/tasks')
+      .get('/api/v1/tasks/no_date_tasks')
       .then((res) => {
-        console.log(res)
+        epics.values = res.data.epics
+        console.log(res.data)
       })
       .catch((e) => {
         console.log(e)
       })
+
+    return { epics }
   },
 })
 </script>
