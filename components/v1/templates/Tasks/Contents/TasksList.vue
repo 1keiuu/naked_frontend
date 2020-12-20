a33136839ca5551c4513ff6ea6afc14f0dddecac
ikkei12 <ikkei12.h@gmail.com>
2020-12-20 14:34:44 +0900

    add content

---
+++ components/v1/templates/Tasks/Contents/TasksList.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>list</p>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({})
+</script>
--- components/v1/templates/Tasks/TasksPage.vue
+++ components/v1/templates/Tasks/TasksPage.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="min-h-screen logIn-page pt-16 flex flex-col justify-center">
     <NkdTaskSubHeader :tabs="contents" @onTabClick="changeContent" />
-    <TasksContent />
+    <TasksIndex
+      v-if="currentTabIndex == 1"
+      :today="today"
+      :tomorrow="tomorrow"
+    />
+    <TasksList v-else-if="currentTabIndex == 2" :epics="epics" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from '@vue/composition-api'
+import { defineComponent, reactive, ref, PropType } from '@vue/composition-api'
 import NkdTaskSubHeader from '@/components/v1/organisms/NkdTasksSubHeader/NkdTasksSubHeader.vue'
-import TasksContent from './TasksContent.vue'
+import TasksIndex from './Contents/index.vue'
+import TasksList from './Contents/list.vue'
+
 export default defineComponent({
   components: {
     NkdTaskSubHeader,
+  },
+  props: {
+    today: {
+      type: Array as PropType<EpicTasks[]>,
+      required: false,
+    },
+    tomorrow: {
+      type: Array as PropType<EpicTasks[]>,
+      required: false,
+    },
+    epics: {
+      type: Array as PropType<EpicTasks[]>,
+      required: false,
+    },
   },
   setup(props, context) {
     const contents = reactive([
       { id: 1, title: '直近のタスク', route: 'index' },
       { id: 2, title: 'リスト', route: 'list' },
     ])
-
     const currentTabIndex = ref(1)
+    const currentPage = context.root.$route.path.replace('/tasks/', '')
+
+    switch (currentPage) {
+      case 'list':
+        currentTabIndex.value = 2
+    }
 
     const changeContent = (id: number, route: string) => {
       currentTabIndex.value = id
