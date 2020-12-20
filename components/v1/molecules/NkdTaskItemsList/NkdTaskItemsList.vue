26b841c0a92de7b19cd37ae123c98a83c136fc7a
ikkei12 <ikkei12.h@gmail.com>
2020-12-20 19:04:22 +0900

    add task imte list

---
+++ components/v1/molecules/NkdTaskItemsList/NkdTaskItemsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NkdTaskItem
+      v-for="task in tasks"
+      :key="'task' + epic.id + '-' + task.id"
+      :task="task"
+    />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+import NkdTaskItem from '@/components/v1/molecules/NkdTaskItem/NkdTaskItem.vue'
+
+export default defineComponent({
+  props: {
+    epic: Object as PropType<Epic>,
+    tasks: {
+      type: Object as PropType<Task[]>,
+    },
+  },
+  setup(props, context) {},
+})
+</script>
---
+++ components/v1/organisms/NkdEpicAccordion/NkdEpicAccordion.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>{{ epic.title }}</h3>
+    <NkdTaskItemsList :epic="epic" :tasks="tasks" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+import NkdTaskItemsList from '@/components/v1/molecules/NkdTaskItemsList/NkdTaskItemsList.vue'
+export default defineComponent({
+  props: {
+    epic: {
+      type: Object as PropType<Epic>,
+    },
+    tasks: {
+      type: Array as PropType<Task[]>,
+    },
+  },
+  setup(props, context) {},
+})
+</script>
