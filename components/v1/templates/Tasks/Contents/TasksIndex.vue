b26356bfc3fdb1241ee487c70b91395e21cecff6
ikkei12 <ikkei12.h@gmail.com>
2020-12-26 18:37:20 +0900

    add no tasks comment

--- components/v1/templates/Tasks/Contents/TasksIndex.vue
+++ components/v1/templates/Tasks/Contents/TasksIndex.vue
@@ -2,7 +2,9 @@
   <div class="min-h-full">
     <div class="epic-tasks__group">
       <h2 class="ml-5 text-xl">今日</h2>
+      <p v-if="today.length <= 0" class="pl-10">タスクはありません</p>
       <NkdEpicTasks
+        v-else-if="today.length >= 1"
         v-for="(epicTasks, i) in today"
         :key="'today-epic-accordion' + i"
         :epic="epicTasks.epic"
@@ -11,7 +13,9 @@
     </div>
     <div class="epic-tasks__group">
       <h2 class="ml-5 text-xl">明日</h2>
+      <p v-if="tomorrow.length <= 0" class="pl-10">タスクはありません</p>
       <NkdEpicTasks
+        v-else-if="tomorrow.length >= 1"
         v-for="(epicTasks, i) in tomorrow"
         :key="'tomorrow-epic-accordion' + i"
         :epic="epicTasks.epic"
@@ -20,7 +24,14 @@
     </div>
     <div class="epic-tasks__group">
       <h2 class="ml-5 text-xl">期日未設定</h2>
+      <p
+        v-if="noDate.length <= 0 && !taskPageStore.isCreatingEpic"
+        class="pl-10"
+      >
+        タスクはありません
+      </p>
       <NkdEpicTasks
+        v-else-if="noDate.length >= 1"
         v-for="(epicTasks, i) in noDate"
         :key="'tomorrow-epic-accordion' + i"
         :epic="epicTasks.epic"
@@ -31,9 +42,9 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from '@vue/composition-api'
+import { defineComponent, PropType, inject } from '@vue/composition-api'
 import NkdEpicTasks from '@/components/v1/organisms/NkdEpicTasks/NkdEpicTasks.vue'
-
+import taskPageStoreKey from '@/components/v1/storeKeys/TaskPageStoreKey'
 export default defineComponent({
   props: {
     today: {
@@ -56,7 +67,8 @@ export default defineComponent({
     const dispatchEvent = (inputValue: string) => {
       context.emit('onInputBlur', inputValue)
     }
-    return { dispatchEvent }
+    const taskPageStore = inject(taskPageStoreKey)
+    return { dispatchEvent, taskPageStore }
   },
 })
 </script>
