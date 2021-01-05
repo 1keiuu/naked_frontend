33f8b8df71dba540aa19e47935e3b330776c530e
ikkei12 <ikkei12.h@gmail.com>
2021-01-05 21:55:15 +0900

    fix lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,10 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'naked_frontend',
+    title: 'NAKED',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
