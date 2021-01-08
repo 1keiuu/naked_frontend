b0aa68ca69c595111aeedfd9f8e76dc913cc8c6b
ikkei12 <ikkei12.h@gmail.com>
2021-01-08 12:42:22 +0900

    fix task input

--- components/v1/molecules/NkdTaskItem/NkdTaskItem.vue
+++ components/v1/molecules/NkdTaskItem/NkdTaskItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="py-2 pl-2 mb-2 bg-white">
-    <p>{{ task.title }}</p>
+    <input class="input" ref="inputRef" @blur="onBlur" @keydown="onKeyDown" />
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from '@vue/composition-api'
+import { defineComponent, onMounted, PropType, ref } from '@vue/composition-api'
 
 export default defineComponent({
   props: {
@@ -12,6 +12,31 @@ export default defineComponent({
       type: Object as PropType<Task>,
     },
   },
-  setup(props, context) {},
+  setup(props, context) {
+    const inputRef = ref<HTMLInputElement>()
+    onMounted(() => {
+      if (!inputRef.value || !props.task) return
+      inputRef.value.value = props.task?.title
+    })
+    const onBlur = () => {
+      const inputValue = inputRef.value?.value
+      context.emit('onInputBlur', inputValue)
+    }
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      const inputValue = inputRef.value?.value
+      if (e.keyCode == 13) {
+        inputRef.value?.blur()
+      }
+    }
+
+    return { inputRef, onBlur, onKeyDown }
+  },
 })
 </script>
+<style scoped>
+.input {
+  padding-left: 4px;
+  max-width: 70%;
+}
+</style>
