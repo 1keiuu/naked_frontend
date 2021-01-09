eb52227c67c0c20117be84f01f8f333b6a586215
ikkei12 <ikkei12.h@gmail.com>
2021-01-09 22:06:35 +0900

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 export default function ({ store, query, redirect }) {
   console.log(query)
   if (query.token) {
-    redirect('/')
     store.$auth.setUserToken(query.token)
+    redirect('/')
   }
 }
