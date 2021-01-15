a3da192aa6e6aa4e3e2230b0bbddfb47f1fb0f59
ikkei12 <ikkei12.h@gmail.com>
2021-01-15 23:19:36 +0900

    install calendar

--- components/v1/organisms/NkdEpicTasks/NkdEpicTasks.vue
+++ components/v1/organisms/NkdEpicTasks/NkdEpicTasks.vue
@@ -3,14 +3,21 @@
     <div class="epic-tasks__inner">
       <h3 class="text-lg">{{ task.title }}</h3>
     </div>
+    <button @click="openCalender" class="open-calendar__button">
+      <NkdIcon type="calendar" color="grey" />
+    </button>
+    <v-calendar v-if="isCalenderOpen" class="calendar" />
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType, inject } from '@vue/composition-api'
+import { defineComponent, PropType, inject, ref } from '@vue/composition-api'
 import NkdTaskItemsList from '@/components/v1/molecules/NkdTaskItemsList/NkdTaskItemsList.vue'
 import TaskPageStoreKey from '@/components/v1/storeKeys/TaskPageStoreKey'
-
+import NkdIcon from '@/components/v1/atoms/NkdIcon/NkdIcon.vue'
 export default defineComponent({
+  components: {
+    NkdIcon,
+  },
   props: {
     task: {
       type: Object as PropType<Task>,
@@ -21,7 +28,7 @@ export default defineComponent({
   },
   setup(props, context) {
     const taskPageStore = inject(TaskPageStoreKey)
-
+    const isCalenderOpen = ref(false)
     const onCardClick = () => {
       const isDrawerOpen = taskPageStore.isDrawerOpen
       if (isDrawerOpen) {
@@ -32,7 +39,31 @@ export default defineComponent({
       taskPageStore.openDrawer()
     }
 
-    return { onCardClick }
+    const openCalender = () => {
+      isCalenderOpen.value = !isCalenderOpen.value
+    }
+
+    return { onCardClick, openCalender, isCalenderOpen }
   },
 })
 </script>
+<style scoped lang="scss">
+.epic-tasks__card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  .open-calendar__button {
+    height: 40px;
+    width: 40px;
+    padding: 10px;
+    border-radius: 50%;
+    border: 1px solid grey;
+  }
+  .calendar {
+    position: absolute;
+    top: 100%;
+    right: 0;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default {
   css: [{ src: '~/assets/scss/global.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['@/plugins/composition-api'],
+  plugins: ['@/plugins/composition-api',{ src: '@/plugins/v-calendar.ts', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -51,7 +51,7 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/auth-next',
-    "@nuxtjs/proxy"
+    "@nuxtjs/proxy",
   ],
   env: {
     NODE_ENV: process.env.NODE_ENV,
