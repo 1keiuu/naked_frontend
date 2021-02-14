c125fe8a4b94d6b9ec7c67a88006e2168a707ffe
中條航紀 <ck227200@gmail.com>
2021-02-14 16:01:21 +0900

    :+1:Add userpage

---
+++ components/v1/templates/User/UserPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="mt-16 pt-12 pl-56 h-screen overflow-scroll"
+  >
+    <div>{{ user.name}}</div>
+    <img
+      src="~/assets/images/avatar.jpg"
+      id="avatar"
+      class="rounded-full h-12 mr-2 cursor-pointer w-12 object-cover"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  ref,
+  computed,
+  PropType,
+} from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    user: {
+      type: Object as PropType<User>,
+    },
+  },
+  components: {},
+  setup(props, context) {
+    console.log(props)
+    console.log(props.user)
+  },
+})
+</script>
--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="task-page w-full h-full">
+    <UserPage
+      :user="state.user"
+    >
+    </UserPage>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, reactive, ref, computed } from '@vue/composition-api'
+import UserPage from '@/components/v1/templates/User/UserPage.vue'
 
 export default defineComponent({
+  components: { UserPage },
   setup(_props, context) {
     const state = reactive({
       user: <User>{},
@@ -16,9 +22,7 @@ export default defineComponent({
     context.root.$axios
       .get(`api/v1/users/${user_id}`)
       .then((res) => {
-        console.log(res)
         state.user = res.data
-        console.log(state.user)
       })
       .catch((e) => {
         console.error(e)
