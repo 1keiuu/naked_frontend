99705986437daecc82b13371519de3cef5ac92c7
1keiuu <ikkei12.h@gmail.com>
2021-05-13 23:35:02 +0900

    fix signin form

--- components/v1/atoms/NkdTextField/NkdTextField.vue
+++ components/v1/atoms/NkdTextField/NkdTextField.vue
@@ -1,18 +1,30 @@
 <template>
-  <input
-    @input="onInput"
-    @blur="onBlur"
-    :placeholder="placeholder"
-    :value="value"
-    :type="type"
-    :name="name"
-    autocomplete
-    :class="{ '--outlined': isOutLined }"
-    class="nkd__text-field w-full p-2 mb-5 rounded-sm border-2 border-opacity-30 border-gray-500 placeholder-opacity-50"
-  />
+  <div class="text-field__wrapper mb-5">
+    <input
+      @input="onInput"
+      @blur="onBlur"
+      :placeholder="placeholder"
+      :value="value"
+      :type="type"
+      :name="name"
+      :maxLength="maxLength"
+      autocomplete
+      :class="{
+        '--outlined': isOutLined,
+        '--error': value && value.length >= maxLength,
+      }"
+      class="nkd__text-field w-full p-2 rounded-sm border-2 border-opacity-30 border-gray-500 placeholder-opacity-50"
+    />
+    <p v-if="isCounter" class="counter">
+      <span :class="{ '--error': value && value.length >= maxLength }">{{
+        value.length
+      }}</span
+      >/{{ maxLength }}
+    </p>
+  </div>
 </template>
 <script lang="ts">
-import { computed } from '@vue/composition-api'
+import { computed, ref } from '@vue/composition-api'
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
@@ -36,6 +48,14 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    maxLength: {
+      type: Number,
+      required: false,
+    },
+    isCounter: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props, context) {
     const onInput = (e: KeyboardEvent) => {
@@ -48,17 +68,37 @@ export default defineComponent({
     const onBlur = (e: Event) => {
       context.emit('onTextFieldBlur', (e.target as HTMLInputElement).value)
     }
+
     return { onInput, onBlur }
   },
 })
 </script>
 <style scoped lang="scss">
+.text-field__wrapper {
+  position: relative;
+}
 .nkd__text-field {
   &.--outlined {
     outline: 0;
     background: transparent;
     border: 0;
     border-bottom: 2px solid rgb(195, 195, 195);
+  }
+  &.--error {
+    border: 2px solid rgb(254, 87, 87);
+  }
+}
+.counter {
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  margin: auto;
+  height: fit-content;
+  color: grey;
+  span {
+    &.--error {
+      color: rgb(254, 87, 87);
+    }
   }
 }
 </style>
