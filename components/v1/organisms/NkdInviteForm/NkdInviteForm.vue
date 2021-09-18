ee159c1f9fefaf7e0d831adcc8e28d011c432b00
中條航紀 <ck227200@gmail.com>
2021-09-18 18:33:00 +0900

    :+1:Add invite_page7

---
+++ components/v1/organisms/NkdInviteForm/NkdInviteForm.vue
@@ -0,0 +1,90 @@
+<template>
+  <form @submit.prevent="onNkdButtonClick()" class="flex flex-col w-full">
+    <NkdTextField
+      type="userName"
+      name="userName"
+      placeholder="userName"
+      :value="SignUpStore.userName"
+      :maxLength="lengthRestrictions['userName']"
+      :isCounter="true"
+      @onTextFieldInput="onInputTextField"
+    />
+    <NkdTextField
+      type="email"
+      name="email"
+      placeholder="email"
+      @onTextFieldInput="onInputTextField"
+    />
+    <NkdTextField
+      type="password"
+      name="password"
+      placeholder="password"
+      :value='SignUpStore.password'
+      :maxLength="lengthRestrictions['password']"
+            :isCounter="true"
+      @onTextFieldInput="onInputTextField"
+    />
+    <NkdButton title="新規登録" type="submit" />
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from '@vue/composition-api'
+import NkdTextField from '../../atoms/NkdTextField/NkdTextField.vue'
+import NkdButton from '../../atoms/NkdButton/NkdButton.vue'
+import SignUpStoreKey from '../../storeKeys/SignUpStoreKey'
+
+export default defineComponent({
+  props: {
+    email: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    userName: {
+      type: String,
+    },
+    companyName: {
+      type: String,
+    },
+  },
+  setup(props, context) {
+    const SignUpStore = inject(SignUpStoreKey)
+    const lengthRestrictions = {
+      userName: 10,
+      password: 20,
+      companyName: 20,
+    }
+    if (!SignUpStore) {
+      throw new Error(`${SignUpStoreKey} is not provided`)
+    }
+
+    const onNkdButtonClick = () => {
+      context.emit('recieveClickEvent')
+    }
+
+    const onInputTextField = (type: string, val: string) => {
+      if (type == 'email') {
+        SignUpStore.setEmail(val)
+      } else if (type == 'password') {
+        if (val.length <= lengthRestrictions['password'])
+          SignUpStore.setPassword(val)
+      } else if (type == 'userName') {
+        if (val.length <= lengthRestrictions['userName'])
+          SignUpStore.setUserName(val)
+      } else if (type == 'companyName') {
+        if (val.length <= lengthRestrictions['companyName'])
+          SignUpStore.setCompanyName(val)
+      }
+    }
+
+    return {
+      SignUpStore,
+      lengthRestrictions,
+      onNkdButtonClick,
+      onInputTextField,
+    }
+  },
+})
+</script>
---
+++ components/v1/templates/SignUp/InvitePage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    class="min-h-screen logIn-page px-8 sm:px-16 lg:px-32 xl:px-64 py-24 flex flex-col justify-center"
+  >
+    <NkdInviteForm @recieveClickEvent="onSubmitButtonClick" />
+    <p
+      class="text-red-500 text-center"
+      v-for="(message, i) in errorMessages"
+      :key="'error-message' + i"
+    >
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, inject } from '@vue/composition-api'
+import SignUpStoreKey from '../../storeKeys/SignUpStoreKey'
+import NkdInviteForm from '../../organisms/NkdInviteForm/NkdInviteForm.vue'
+
+export default defineComponent({
+  components: {
+    NkdInviteForm,
+  },
+  setup(_props, context) {
+    const errorMessages = reactive<string[]>([])
+    const SignUpStore = inject(SignUpStoreKey)
+
+    if (!SignUpStore) {
+      throw new Error(`${SignUpStoreKey} is not provided`)
+    }
+    // SignUpStore.setEmail(route.query.email)
+    SignUpStore.setEmail(context.root.$route.query.email)
+
+    const onSubmitButtonClick = async () => {
+      errorMessages.splice(0, errorMessages.length)
+
+      await context.root.$axios
+        .post('/api/v1/users/update_invite', {
+          user: {
+            email: SignUpStore.email,
+            password: SignUpStore.password,
+            name: SignUpStore.userName,
+          },
+        })
+        .then((response) => {
+          context.root.$auth.setUser(response.data.user)
+          context.root.$auth.setUserToken(response.data.user.token)
+        })
+        .catch((error) => {
+          console.log(error.response)
+          if (!error.response.data.message) {
+            errorMessages.push('新規登録に失敗しました')
+            return
+          }
+          error.response.data.message.forEach((message: string) =>
+            errorMessages.push(message)
+          )
+        })
+    }
+
+    return { errorMessages, onSubmitButtonClick }
+  },
+})
+</script>
